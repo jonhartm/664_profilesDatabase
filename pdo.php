e23f3440307340448ca5c2eb4a1ea508dbc0c20e
jonhartm <jonhartm@umich.edu>
2018-03-17 17:40:46 -0400

    added error chatching in pdo in case the database isn't set up correctly

--- pdo.php
+++ pdo.php
@@ -1,3 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc','Jon','bears');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+  $pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc','Jon','bears');
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
