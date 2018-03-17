5fe24a9592f0070513adc7e43bf454f6425ed29e
jonhartm <jonhartm@umich.edu>
2018-03-17 16:49:40 -0400

    added simple name search via form -> get parameter

--- index.php
+++ index.php
@@ -2,7 +2,27 @@
 require_once "pdo.php";
 require_once "util.php";
 
-session_start()
+session_start();
+
+if (isset($_POST['search'])) {
+  if (strlen($_POST['filter_by']) > 0) {
+    $_SESSION['success'] = "Search results for names containing \"{$_POST['filter_by']}\"";
+    header("Location: index.php?filter={$_POST['filter_by']}");
+    return;
+  } else {
+    header("Location: index.php");
+    return;
+  }
+}
+
+if (isset($_GET['filter'])) {
+  $sql = 'SELECT profile_id, user_id, first_name, last_name, headline FROM profile WHERE first_name LIKE "%'.$_GET['filter'].'%" OR last_name LIKE "%'.$_GET['filter'].'%"';
+} else {
+  $sql = 'SELECT profile_id, user_id, first_name, last_name, headline FROM profile';
+}
+
+$stmt = $pdo->query($sql);
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
  ?>
 
@@ -22,19 +42,30 @@ session_start()
 <div class="container">
 <h1>Jonathan Hartman's Resume Registry</h1>
 <?php
-  flashMessages();
 if (!isset($_SESSION['user_id'])) {
   echo '<p><a href="login.php">Please log in</a></p>';
 } else {
   echo "<h2>Hello {$_SESSION['name']}</h2>";
   echo '<p><a href="logout.php">Log out</a></p>';
 }
-$stmt = $pdo->query("SELECT profile_id, user_id, first_name, last_name, headline FROM profile");
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+flashMessages();
+
+echo '<form method="post">';
+echo '<p>Search:';
+echo '<input type="text" name="filter_by" value="" size="20"/>';
+echo '<input type="submit" name="search" value="Search">';
+echo '</form>';
+if (isset($_GET['filter'])) {
+  echo '<form action="index.php">';
+  echo '<input type="submit" value="Clear Search" />';
+  echo '</form>';
+}
+echo '</p>';
 
 if (!$rows) {
   echo '<p>No Profiles Found</p>';
 } else {
+  // Search Form
   echo '<table border="1">';
   echo '<tr><th>Name</th><th>Headline</th><th>Action</th><tr>';
   foreach ($rows as $row) {
