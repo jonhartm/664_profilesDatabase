ed5fd8d7a5331b099a032c3d7cded9f5a5f80009
jonhartm <jonhartm@umich.edu>
2018-03-24 15:35:54 -0400

    moved bootstrap/jquery to their own php file that gets included in the html head

--- index.php
+++ index.php
@@ -52,13 +52,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <html>
 <head>
 <title>Jonathan Hartman's Resume Registry</title>
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
+<?php include "head.php"; ?>
 </head>
 <body>
 <div class="container">
